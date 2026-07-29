d8b8006f7787e89d893db10c234ac55f86b0c727
WinterSunst95 <wintersunset95@gmail.com>
2026-07-29 22:39:08 +0530

    feat: initial commit for project

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Juno's wayland compositor dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        rustc
+        cargo
+        rustfmt
+        rust-analyzer
+      ];
+
+      buildInputs = with pkgs; [
+        wayland
+        wayland-protocols
+        wayland-scanner
+        libxkbcommon
+        libinput
+        mesa
+        seatd
+        udev
+      ];
+
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+        wayland
+        libxkbcommon
+        libinput
+        mesa
+        udev
+      ]);
+    };
+  };
+}
